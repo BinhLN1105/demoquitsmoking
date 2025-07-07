84505a9b9126a884f8fccfb23e06bb76c825d6e9
BinhLN1105 <luubinhbeo@gmail.com>
2025-07-07 19:48:25 +0700

    fix be1

--- QuitSmoking_BE/quitsmoking/src/main/java/com/quitsmoking/config/SecurityConfig.java
+++ QuitSmoking_BE/quitsmoking/src/main/java/com/quitsmoking/config/SecurityConfig.java
@@ -138,8 +138,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
                 "http://localhost:4173",
-                "http://localhost:5173"
-
+                "http://localhost:5173",
+                "https://demoquitsmoking-frontend.onrender.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
